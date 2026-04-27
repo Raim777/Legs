56fa076f6606511dbc1c44e039c458b15c3c0f36
Raim777 <rako2002@mail.ru>
2026-04-27 17:46:22 +0500

    Upgraded: TPS play mode tests

--- Assets/Tests/Runtime/ThirdPersonControllerPlayModeTests.cs
+++ Assets/Tests/Runtime/ThirdPersonControllerPlayModeTests.cs
@@ -1,6 +1,6 @@
 using System.Collections;
-using System.Reflection;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -8,30 +8,29 @@ namespace Character.Tests
 {
     public class ThirdPersonControllerPlayModeTests
     {
-        private GameObject _floor;
-        private GameObject _cameraGO;
-        private GameObject _characterGO;
+        private const string TestScenePrefabPath = "Assets/Tests/Editor/ControllerTest.prefab";
+
+        private GameObject _root;
+        private ScriptedCharacterInput _input;
+        private Transform _playerTransform;
 
         [TearDown]
         public void TearDown()
         {
-            if (_floor != null) Object.Destroy(_floor);
-            if (_cameraGO != null) Object.Destroy(_cameraGO);
-            if (_characterGO != null) Object.Destroy(_characterGO);
+            if (_root != null) Object.Destroy(_root);
         }
 
         [UnityTest]
         public IEnumerator Character_MovesForwardThenLeft_WithScriptedInput()
         {
             BuildScene();
-            var input = _characterGO.GetComponent<ScriptedCharacterInput>();
 
             // let Start() run on all components
             yield return null;
 
-            Vector3 start = _characterGO.transform.position;
+            Vector3 start = _playerTransform.position;
 
-            input.Play(new[]
+            _input.Play(new[]
             {
                 InputCommand.Forward(5f),
                 InputCommand.Left(2f),
@@ -39,7 +38,7 @@ namespace Character.Tests
 
             yield return RunFor(7.2f);
 
-            Vector3 delta = _characterGO.transform.position - start;
+            Vector3 delta = _playerTransform.position - start;
 
             // MoveSpeed = 2 m/s with short ramp-up; forward 5s should push +Z well past 6m
             Assert.Greater(delta.z, 9.7f, $"Expected forward (+Z) travel > 9.7m, got {delta}");
@@ -51,17 +50,16 @@ namespace Character.Tests
         public IEnumerator Character_StaysStill_WhenInputIsIdle()
         {
             BuildScene();
-            var input = _characterGO.GetComponent<ScriptedCharacterInput>();
 
             yield return null;
 
-            Vector3 start = _characterGO.transform.position;
+            Vector3 start = _playerTransform.position;
 
-            input.Play(new[] { InputCommand.Idle(1.5f) });
+            _input.Play(new[] { InputCommand.Idle(1.5f) });
 
             yield return RunFor(1.7f);
 
-            Vector3 delta = _characterGO.transform.position - start;
+            Vector3 delta = _playerTransform.position - start;
             Assert.Less(Mathf.Abs(delta.x), 0.1f, $"No X drift expected, got {delta.x}");
             Assert.Less(Mathf.Abs(delta.z), 0.1f, $"No Z drift expected, got {delta.z}");
         }
@@ -70,33 +68,31 @@ namespace Character.Tests
         public IEnumerator ScriptedInput_FiresFinished_AndStopsPlaying()
         {
             BuildScene();
-            var input = _characterGO.GetComponent<ScriptedCharacterInput>();
 
             yield return null;
 
             bool finished = false;
-            input.Finished += () => finished = true;
-            input.Play(new[] { InputCommand.Forward(0.5f) });
+            _input.Finished += () => finished = true;
+            _input.Play(new[] { InputCommand.Forward(0.5f) });
 
             yield return RunFor(0.9f);
 
             Assert.IsTrue(finished, "Finished event should have fired");
-            Assert.IsFalse(input.IsPlaying, "IsPlaying should be false after Stop mode finished");
+            Assert.IsFalse(_input.IsPlaying, "IsPlaying should be false after Stop mode finished");
         }
 
         [UnityTest]
         public IEnumerator ScriptedInput_LoopsWhenConfigured()
         {
             BuildScene();
-            var input = _characterGO.GetComponent<ScriptedCharacterInput>();
 
             yield return null;
 
-            input.Play(new[] { InputCommand.Forward(0.3f) }, ScriptedInputEndMode.Loop);
+            _input.Play(new[] { InputCommand.Forward(0.3f) }, ScriptedInputEndMode.Loop);
 
             yield return RunFor(1.2f);
 
-            Assert.IsTrue(input.IsPlaying, "Loop mode should keep IsPlaying true");
+            Assert.IsTrue(_input.IsPlaying, "Loop mode should keep IsPlaying true");
         }
 
         private static IEnumerator RunFor(float seconds)
@@ -111,46 +107,12 @@ namespace Character.Tests
 
         private void BuildScene()
         {
-            _floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            _floor.name = "TestFloor";
-            _floor.transform.position = new Vector3(0f, -0.5f, 0f);
-            _floor.transform.localScale = new Vector3(100f, 1f, 100f);
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(TestScenePrefabPath);
+            Assert.NotNull(prefab, $"Test scene prefab not found at {TestScenePrefabPath}");
 
-            _cameraGO = new GameObject("TestCamera");
-            _cameraGO.transform.position = new Vector3(0f, 5f, -5f);
-            var cam = _cameraGO.AddComponent<Camera>();
-
-            _characterGO = new GameObject("TestCharacter");
-            _characterGO.transform.position = new Vector3(0f, 1f, 0f);
-
-            var cmTarget = new GameObject("CinemachineCameraTarget");
-            cmTarget.transform.SetParent(_characterGO.transform);
-            cmTarget.transform.localPosition = new Vector3(0f, 1.6f, 0f);
-
-            var controller = _characterGO.AddComponent<CharacterController>();
-            controller.radius = 0.28f;
-            controller.height = 1.8f;
-            controller.center = new Vector3(0f, 0.93f, 0f);
-
-            var motor = _characterGO.AddComponent<CharacterMotor>();
-            var cameraRig = _characterGO.AddComponent<CameraRig>();
-            var tpc = _characterGO.AddComponent<ThirdPersonController>();
-            var input = _characterGO.AddComponent<ScriptedCharacterInput>();
-
-            motor.moveSpeed = 2f;
-
-            SetPrivateField(motor, "mainCamera", cam);
-            SetPrivateField(motor, "groundLayers", (LayerMask)~0);
-            SetPrivateField(cameraRig, "cinemachineCameraTarget", cmTarget);
-
-            tpc.SetInput(input);
-        }
-
-        private static void SetPrivateField(object target, string name, object value)
-        {
-            var field = target.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
-            Assert.NotNull(field, $"Field '{name}' not found on {target.GetType().Name}");
-            field.SetValue(target, value);
+            _root = Object.Instantiate(prefab);
+            _input = _root.GetComponentInChildren<ScriptedCharacterInput>();
+            _playerTransform = _root.GetComponentInChildren<CharacterMotor>().transform;
         }
     }
 }
